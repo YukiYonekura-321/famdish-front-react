16cd139bd20f7179209817c6ec1ce42b84218555
YukiYonekura <yoneyuu321@gmail.com>
2025-11-19 07:52:33 +0900

    🚸フッターの削除とレイアウトの微修正

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,5 +1,4 @@
 import { Header } from "../../components/header";
-import { Footer } from "../../components/footer";
 
 // LoginPage というReactコンポーネントを定義
 // ボタンを表示して、クリックすると login 関数が呼ばれ、Googleログイン開始
@@ -8,15 +7,19 @@ export default function AboutPage() {
     <div>
       <Header />
 
-      <div className="flex flex-col justify-center items-center mt-4 space-y-4">
-        <h1 className="text-2xl font-bold">FamDishとは</h1>
-        <p>
-          献立を考える手間をなくしたい。食べたいものが食卓に出てくると嬉しい。食材の無駄やマンネリ化をなくしたい。そんな願いを叶えます。
+      <div className="min-h-screen flex flex-col justify-center items-center bg-cyan-400">
+        <h1 className="text-white text-6xl font-bold tracking-widest drop-shadow-lg">
+          FamDishとは
+        </h1>
+
+        <p className="text-white inline-block w-1/2 text-center drop-shadow-lg">
+          献立を考える手間をなくしたい。食べたいものが食卓に出てくると嬉しい。食材の無駄やマンネリ化をなくしたい。
+        </p>
+
+        <p className="text-white inline-block w-1/2 text-center drop-shadow-lg">
+          そんな願いを叶えます。
         </p>
       </div>
-
-      <Footer />
     </div>
   );
-  // return <button onClick={login}>Googleでログイン</button>;
 }
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -7,8 +7,6 @@ import { GoogleAuthProvider, signInWithRedirect } from "firebase/auth";
 // lib/firebase.ts で初期化した Firebase 認証オブジェクト auth をインポート。これを使ってログイン処理を実行
 import { auth } from "./lib/firebase";
 import { Header } from "../components/header";
-import { Footer } from "../components/footer";
-// import { useRef } from "react";
 import { useEffect, useState } from "react";
 
 // GoogleAuthProvider() を使って Googleログイン用のプロバイダーを作成。
@@ -25,18 +23,7 @@ const bgImages = [
   "/istockphoto-1442729474-612x612.jpg",
 ];
 
-// LoginPage というReactコンポーネントを定義
-// ボタンを表示して、クリックすると login 関数が呼ばれ、Googleログイン開始
 export default function LoginPage() {
-  // const targetRef = useRef(null);
-
-  // // ボタンを押したら target の class を切り替える処理
-  // const handleToggle = () => {
-  //   if (targetRef.current) {
-  //     targetRef.current.classList.toggle("invisible");
-  //     targetRef.current.classList.toggle("opacity-0");
-  //   }
-  // };
   const [bgIndex, setBgIndex] = useState(0);
 
   useEffect(() => {
@@ -51,13 +38,7 @@ export default function LoginPage() {
     <div>
       <Header />
 
-      <div
-        className="relative w-full min-h-screen p-8"
-        // style={{
-        //   backgroundImage: `url(${bgImages[bgIndex]})`,
-        //   opacity: fade ? 1 : 0,
-        // }}
-      >
+      <div className="relative w-full min-h-screen p-8">
         {/* 背景画像3枚重ねる 画像を重ねないと文字も消えてしまう */}
         {bgImages.map((img, idx) => (
           <div
@@ -71,9 +52,9 @@ export default function LoginPage() {
         ))}
         {/* マスク（暗くする） */}
         <div className="absolute inset-0 bg-black/50"></div>
-        <div className="absolute top-1/2 -translate-y-1/2 left-1/2 -translate-x-1/2 z-10 flex-col items-center justify-center text-center space-y-8">
-          <h1 className="text-4xl font-bold text-white drop-shadow-lg">
-            食卓で家族は繋がる!!!
+        <div className="absolute top-1/2 -translate-y-1/2 left-1/2 -translate-x-1/2 z-10 flex-col text-center space-y-8">
+          <h1 className="text-6xl font-bold text-white drop-shadow-lg">
+            食卓で家族は繋がる
           </h1>
 
           <button
@@ -84,33 +65,8 @@ export default function LoginPage() {
           >
             Googleでログイン
           </button>
-
-          <h1 className="text-2xl font-bold tracking-widest text-white drop-shadow-lg">
-            FamDishとは
-          </h1>
-
-          <p className="text-white drop-shadow-lg">
-            献立を考える手間をなくしたい。食べたいものが食卓に出てくると嬉しい。食材の無駄やマンネリ化をなくしたい。そんな願いを叶えます。
-          </p>
         </div>
       </div>
-
-      <Footer />
     </div>
   );
-  // return <button onClick={login}>Googleでログイン</button>;
 }
-
-// bg-[url('../../public/top.jpg
-
-// .top-main h2 {
-//   font-size: 70px;
-//   font-weight: 500;
-//   line-height: 1.3;
-//   -webkit-font-smoothing: antialiased;
-//   margin-bottom: 20px;
-// }
-
-// .top-main p {
-//   font-size: 24px;
-// }
