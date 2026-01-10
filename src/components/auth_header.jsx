3126c9e78318b197c78f3d7cd502b4c846facea6
YukiYonekura <yoneyuu321@gmail.com>
2026-01-10 14:30:47 +0900

    ✨ ログアウト機能を実装

--- src/components/auth_header.jsx
+++ src/components/auth_header.jsx
@@ -1,6 +1,22 @@
+"use client";
+
 import Link from "next/link";
+import { signOut } from "firebase/auth";
+import { useRouter } from "next/navigation";
+import { auth } from "@/app/lib/firebase";
 
 export function AuthHeader() {
+  const router = useRouter();
+
+  const logout = async () => {
+    try {
+      await signOut(auth);
+      router.replace("/");
+    } catch (err) {
+      console.error("❌ Logout failed", err);
+    }
+  };
+
   return (
     <header className="flex justify-between items-center h-16 fixed top-0 left-0 w-full bg-zinc-800/50">
       <Link className="p-4 text-white text-[32px] mr-16" href="/">
@@ -21,12 +37,12 @@ export function AuthHeader() {
           リクエスト
         </Link>
 
-        <Link
-          className="text-white block leading-16 px-4 bg-gray-600/50 mr-8 hover:bg-gray-500/50 transition duration-500"
-          href="/about"
+        <button
+          onClick={logout}
+          className="text-white block leading-16 px-4 bg-gray-600/60 hover:bg-gray-500/60 transition duration-500 mr-8"
         >
           ログアウト
-        </Link>
+        </button>
       </ul>
     </header>
   );
