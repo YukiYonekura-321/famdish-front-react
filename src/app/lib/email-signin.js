563138251d9c90e74a51ed0aadd283190ece1b7d
YukiYonekura <yoneyuu321@gmail.com>
2026-01-14 06:29:46 +0900

    ✨emailサインイン機能追加

---
+++ src/app/lib/email-signin.js
@@ -0,0 +1,19 @@
+import { signInWithEmailLink } from "firebase/auth";
+import { auth } from "@/app/lib/firebase";
+import { getLinkedProviderIds } from "@/app/lib/provider-utils";
+
+export const handleEmailSignIn = async () => {
+  const email = window.prompt("確認のためメールアドレスを入力してください");
+
+  try {
+    const result = await signInWithEmailLink(auth, email, window.location.href);
+    const linkedProviderIds = getLinkedProviderIds(result.user);
+
+    // 仮登録中のメールアドレスでメールリンクログインをするとFirebase Authの仕様でIdPの連携が解除される
+    if (linkedProviderIds.length === 0) {
+      alert("メールが使えない場合に備えて、\nIdPとの連携を行ってください。");
+    }
+  } catch (error) {
+    alert(`ログインに失敗しました:${error.message}`);
+  }
+};
