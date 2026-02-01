93296fe257802aa7499ea675f685620b160ef52a
YukiYonekura <yoneyuu321@gmail.com>
2026-02-01 15:33:55 +0900

    🚸 モバイル対応のヘッダーメニューを追加

--- src/components/MobileAuthMenuItems.jsx
+++ src/components/MobileAuthMenuItems.jsx
@@ -1,3 +1,5 @@
+"use client";
+import Link from "next/link";
 import MobileNavLink from "./MobileNavLink";
 import { auth } from "@/app/lib/firebase";
 import { signOut } from "firebase/auth";
@@ -19,13 +21,57 @@ export default function MobileAuthMenuItems({ onClick }) {
       <MobileNavLink href="/members" onClick={onClick}>
         メンバー登録
       </MobileNavLink>
+      <MobileNavLink href="/members/index" onClick={onClick}>
+        メンバー一覧
+      </MobileNavLink>
       <MobileNavLink href="/menus" onClick={onClick}>
         リクエスト
       </MobileNavLink>
+      <MobileNavLink href="/menus/index" onClick={onClick}>
+        リクエスト一覧
+      </MobileNavLink>
+      <div className="relative group">
+        <div className="text-white">マイページ設定</div>
 
-      <button onClick={logout} className="text-white text-left">
-        ログアウト
-      </button>
+        {/* プルダウンメニュー */}
+        <div
+          className="
+              absolute right-0 mt-0 w-56
+              hidden group-hover:block
+              bg-zinc-800/95 backdrop-blur
+              shadow-lg rounded-md
+              py-2
+            "
+        >
+          <Link
+            href="/mypage/social"
+            className="block px-4 py-2 text-sm text-white hover:bg-zinc-700 transition"
+          >
+            ソーシャルアカウント連携状態
+          </Link>
+
+          <Link
+            href="/mypage/email"
+            className="block px-4 py-2 text-sm text-white hover:bg-zinc-700 transition"
+          >
+            通知先メールアドレス変更
+          </Link>
+
+          <button
+            onClick={logout}
+            className="w-full text-left px-4 py-2 text-sm text-white hover:bg-zinc-700 transition"
+          >
+            ログアウト
+          </button>
+
+          <Link
+            href="/mypage/withdraw"
+            className="block px-4 py-2 text-sm text-red-400 hover:bg-zinc-700 transition"
+          >
+            退会
+          </Link>
+        </div>
+      </div>
     </>
   );
 }
--- src/components/MobileMenuItems.jsx
+++ src/components/MobileMenuItems.jsx
@@ -3,11 +3,14 @@ import MobileNavLink from "./MobileNavLink";
 export default function MobileMenuItems({ onClick }) {
   return (
     <>
-      <MobileNavLink href="/about" onClick={onClick}>
+      <MobileNavLink href="/" onClick={onClick}>
         FamDishとは
       </MobileNavLink>
-      <MobileNavLink href="/members" onClick={onClick}>
+      <MobileNavLink href="/sign-in" onClick={onClick}>
         新規登録
+      </MobileNavLink>
+      <MobileNavLink href="/login" onClick={onClick}>
+        ログイン
       </MobileNavLink>
     </>
   );
