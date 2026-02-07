2be3b2ec706d98a9a32c8baeda26fb1cccb189b1
YukiYonekura <yoneyuu321@gmail.com>
2026-02-07 18:39:42 +0900

    🐛 auth.currentUserでログイン済みか判定

--- src/app/sign-in/page.jsx
+++ src/app/sign-in/page.jsx
@@ -19,9 +19,17 @@ export default function LoginPage() {
 
   // ログイン済みなら新規登録ページに来ても /menus にリダイレクト
   useEffect(() => {
+    // 既に認証済みの場合は即時リダイレクト
+    if (auth.currentUser) {
+      router.replace("/menus");
+      return;
+    }
+
     const unsub = onAuthStateChanged(auth, (user) => {
       if (user) {
         router.replace("/menus");
+        console.log("onAuthStateChanged user:", user);
+        console.log("calling replace");
       }
     });
     return () => unsub();
