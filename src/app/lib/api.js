d5dccb25c006f5e140ba4f205e56ff8f9bec6873
YukiYonekura <yoneyuu321@gmail.com>
2026-02-25 08:19:28 +0900

    🔥 使用していないコード削除

--- src/app/lib/api.js
+++ src/app/lib/api.js
@@ -1,6 +1,5 @@
 import axios from "axios";
-import { auth } from "./firebase";
-import { onAuthStateChanged } from "firebase/auth";
+import { auth } from "@/app/lib/firebase";
 
 // 決定する baseURL を先に計算
 const baseURL = process.env.NEXT_PUBLIC_API_URL || "";
@@ -23,7 +22,6 @@ export const apiClient = axios.create({
 apiClient.interceptors.request.use(
   async (config) => {
     const user = auth.currentUser;
-    console.log(user);
     if (user) {
       const token = await user.getIdToken();
       config.headers.Authorization = `Bearer ${token}`;
@@ -32,26 +30,3 @@ apiClient.interceptors.request.use(
   },
   (error) => Promise.reject(error),
 );
-
-export const fetchMenu = async () => {
-  return new Promise((resolve, reject) => {
-    onAuthStateChanged(auth, async (user) => {
-      if (!user) {
-        console.error("未ログインです");
-        console.log(user);
-        reject(new Error("未ログインです"));
-        return;
-      }
-
-      try {
-        const token = await user.getIdToken(); // 引数の`true`を消すと解決
-        console.log("取得したトークン:", token); // ← ここで確認
-        const res = await apiClient.get("/api/menus");
-        resolve(res.data);
-      } catch (err) {
-        console.error("API呼び出しエラー:", err);
-        reject(err);
-      }
-    });
-  });
-};
